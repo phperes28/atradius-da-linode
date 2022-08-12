1754861312d7c6b0dca87e2d0f5c941421c0b28d
phperes28 <84190778+phperes28@users.noreply.github.com>
2022-08-12 13:09:56 +1000

    Changes in script message, allowed local host in settings

--- DA_generator/scripts.py
+++ DA_generator/scripts.py
@@ -12,11 +12,13 @@ da_type = (
 
 
 def generate_first_contact(buyer_number, buyer_name, contact_name,  supplier, fins_1, fins_2, ):
+    #IF fins_2 == Nul - use another script
     return (f"""Atradius Information Request -  {buyer_name}// Atradius Ref. {buyer_number} \n
 
 Hi {contact_name}, 
 
 I hope you are doing well. 
+
 We are conducting a review on {buyer_name} in consideration of an application for trade credit insurance cover received from your supplier {supplier}.\n
 In order for us to assist your supplier with insurance cover, we would appreciate it if you could please provide the following: \n
 - {fins_1}\n
@@ -44,25 +46,25 @@ def generate_annual_review_with_supplier(buyer_number, buyer_name, contact_name,
     I hope this email find you well.\n
     
     You spoke to my colleague, {previous_contact} previously and you kindly sent through confidential financials for {buyer_name} (refer to correspondence below).\n
-    Would you please assist us once again as we are conducting an annual review on {buyer_name} and we require updated financial information so we are able to make a proper risk
-    assessment. Based on this information we will decide whether to roll over your status as covered buyer in our database.\n 
+    Would you please assist us once again as we are conducting an annual review on {buyer_name} and we require updated financial information so we are able to make a proper risk assessment.\n
+    Based on this information we will decide whether to roll over your status as covered buyer in our database.\n 
     Can you kindly provide the following:\n
     - {fins_1}\n
     - {fins_2}\n
     
-    If the audited numbers are unavailable, we would appreciate any draft or interim management accounts.\n
+    If the audited numbers are unavailable, we would appreciate any draft or interim management accounts.
     
     All information supplied to us is treated on a confidential basis and is for internal assessment purposes only and is not passed onto a third party without your prior consent.
-     I am happy to provide a confidentiality agreement, please advise if this is required.\n
+     I am happy to provide a confidentiality agreement, please advise if this is required.
     
-    About Atradius:\n
+    About Atradius:
     Atradius, one of the world's leading credit insurance and credit management companies, protects more than EUR400 billion of the world trade annually against the risks of non-payment.
     Head-quartered in Amsterdam, the group has a presence in more than 50 countries on five continents, employing around 3,700 people with and annual turnover of EUR1.8 billion, giving 
     us a 24% per cent global market share. Our company which has a wealth of knowledge on trade sectors, countries and business trends, makes more than 20,000 credit decisions daily.\n
     
     Should you have any queries or wish to further discuss the details, please do not hesitate to contact me.\n
     
-    Thank you, \n
+    Thank you, 
     
     Pedro Peres""")
 
